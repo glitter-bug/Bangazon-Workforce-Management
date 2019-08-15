52fad93a69603bd9f56e4ea17e216d6e04860be6
Deep-A_Patel <deep.ag123007@gmail.com>
2019-08-15 12:53:18 -0700

    Models and Nev bar

--- Bangazon-Workforce-Management/Models/Customer.cs
+++ Bangazon-Workforce-Management/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Bangazon_Workforce_Management.Models
@@ -12,6 +13,10 @@ namespace Bangazon_Workforce_Management.Models
 
         [Required]
         public string LastName { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastActiveTime { get; set; }
 
         public List<PaymentType> PaymentTypes { get; set; } = new List<PaymentType>();
 
