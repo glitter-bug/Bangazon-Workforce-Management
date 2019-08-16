75f097f2f89b5b4508b52e64404a476c516601e4
Joey <driscoll.recording@gmail.com>
2019-08-15 21:38:54 -0500

    need to create view model for employee details. moving on to create

--- Bangazon-Workforce-Management/Controllers/EmployeesController.cs
+++ Bangazon-Workforce-Management/Controllers/EmployeesController.cs
@@ -62,7 +62,7 @@ namespace Bangazon_Workforce_Management.Controllers
         }
 
         // GET: Employees/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int id, Computer computer, Department department, TrainingProgram trainingProgram)
         {
             Employee employee = null;
             using (SqlConnection conn = Connection)
@@ -71,9 +71,12 @@ namespace Bangazon_Workforce_Management.Controllers
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                        SELECT Id, FirstName, LastName, DepartmentId, IsSuperVisor
-                        FROM Employee
-                        WHERE Id = @id
+                       SELECT e.FirstName, e.LastName, d.Name AS DepartmentName, c.Make AS ComputerMake, c.Manufacturer As ComputerManufacturer, tp.Name AS TrainingProgram
+                    FROM Department d 
+                    JOIN Employee e ON d.Id = e.DepartmentId
+                    JOIN Computer c ON e.Id = c.Id
+                    JOIN TrainingProgram tp ON c.Id = tp.Id
+                    WHERE e.Id = @id
                     ";
                     cmd.Parameters.Add(new SqlParameter("@id", id));
                     SqlDataReader reader = cmd.ExecuteReader();
@@ -82,11 +85,21 @@ namespace Bangazon_Workforce_Management.Controllers
                     {
                         employee = new Employee()
                         {
-                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
                             FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
                             LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
-                            IsSuperVisor = reader.GetBoolean(reader.GetOrdinal("IsSuperVisor"))
+                        };
+                        computer = new Computer()
+                        {
+                            Make = reader.GetString(reader.GetOrdinal("ComputerMake")),
+                            Manufacturer = reader.GetString(reader.GetOrdinal("ComputerManufacturer")),
+                        };
+                        department = new Department()
+                        {
+                            Name = reader.GetString(reader.GetOrdinal("DepartmentName")),
+                        };
+                        trainingProgram = new TrainingProgram()
+                        {
+                            Name = reader.GetString(reader.GetOrdinal("TrainingProgram")),
                         };
                     }
                 }
