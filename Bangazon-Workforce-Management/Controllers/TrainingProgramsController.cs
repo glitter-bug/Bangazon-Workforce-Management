1a816aea5f029ef0b331dbd07ec005093fe931b0
joab sigrah <jksiggy@gmail.com>
2019-08-15 22:24:25 -0500

    save

--- Bangazon-Workforce-Management/Controllers/TrainingProgramsController.cs
+++ Bangazon-Workforce-Management/Controllers/TrainingProgramsController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Bangazon_Workforce_Management.Models;
+using Bangazon_Workforce_Management.Models.ViewModels;
+
 namespace Bangazon_Workforce_Management.Controllers
 {
     public class TrainingProgramsController : Controller
@@ -79,11 +81,11 @@ namespace Bangazon_Workforce_Management.Controllers
                     {
                         trainingProgram = new TrainingProgram()
                         {
-                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                           Id = reader.GetInt32(reader.GetOrdinal("Id")),
                             Name = reader.GetString(reader.GetOrdinal("Name")),
                             StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
                             EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
-                            MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees"))
+                            MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees")) 
                         };
                     }
                 }
@@ -92,9 +94,55 @@ namespace Bangazon_Workforce_Management.Controllers
             return View(trainingProgram);
         }
         // GET: TrainingPrograms/Create
+        [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new TrainingProgramCreateViewModel(_config.GetConnectionString("DefaultConnection"));
+            return View(viewModel);
+        }
+
+        // POST: TrainingPrograms/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TrainingProgram trainingProgram)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"
+                            INSERT INTO TrainingProgram (
+                                Name,
+                                StartDate,
+                                EndDate,
+                                MaxAttendees
+                                ) VALUES (
+                                @name,
+                                @startDate,
+                                @endDate,
+                                @maxAttendees
+                            )
+                        ";
+
+                        cmd.Parameters.AddWithValue("@name", trainingProgram.Name);
+                        cmd.Parameters.AddWithValue("@startDate", trainingProgram.StartDate);
+                        cmd.Parameters.AddWithValue("@endDate", trainingProgram.EndDate);
+                        cmd.Parameters.AddWithValue("@maxAttendees", trainingProgram.MaxAttendees);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // POST: TrainingPrograms/Create
